21bc6c137ab9beeefe6bddcc042855722714efaf
Szymon <tabolator1@gmail.com>
2025-12-30 20:45:14 +0100

    lista pytań i dodawanie pytań ze strony

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import cors from 'cors'
 import formidable from 'formidable'
 import fs from 'fs'
 import qrcode from 'qrcode'
-import pytania from './static/pytanka.json' with { type: "json" };
+import pytanka from './static/pytanka.json' with { type: "json" };
+let pytania = pytanka.pytania
 const __dirname = import.meta.dirname
 const uploadPath = path.join(__dirname,"static","upload")
 // const express = require('express')
@@ -23,6 +24,7 @@ app.set('view engine', 'hbs')
 let playerlist = [
 ]
 const adminpassword="123"
+let endPoll = false
 
 app.get("/", (req, res) => {
     res.render("login.hbs")
@@ -42,17 +44,54 @@ app.post("/login", (req, res) => {
 })
 app.post("/buttonClicked",(req,res)=>{
     const id = Number(req.body.id)
-    if (!playerlist[id].button){
+    if (!endPoll){
+        endPoll = true
         playerlist[id].button=true
         console.log(playerlist[id].name)
-    }
-})
 
+        // if (!playerlist[id].button){
+        //     playerlist[id].button=true
+        // }
+    }
+    console.log(playerlist[id].name, playerlist[id].button, endPoll)
+    res.send("ok")
+})
+app.get("/addQuestions",(req,res)=>{
+    if(req.query.tresc!=undefined){
+        // console.log(`,
+        // {
+        //     "nr":${pytania.length+1},
+        //     "kategoria":"${req.query.kategoria}",
+        //     "tresc":"${req.query.tresc}",
+        //     "poprawna odp":"${req.query["poprawna odp"]}"
+        // }`)
+        fs.appendFile("./static/pytanka2.json",`,
+        {
+            "nr":${pytania.length+1},
+            "kategoria":"${req.query.kategoria}",
+            "tresc":"${req.query.tresc}",
+            "poprawna odp":"${req.query["poprawna odp"]}"
+        }`,(err)=>{
+            if(err) console.log(err)
+        })
+        pytania.push({
+            nr:pytania.length+1,
+            kategoria:req.query.kategoria,
+            tresc:req.query.tresc,
+            'poprawna odp':req.query["poprawna odp"]
+        })
+    }
+    // console.log(pytania)
+    // console.log(pytanka)
+    res.render("pytanie.hbs")
+})
 app.post("/buttonUnclicked",(req,res)=>{
     console.log("-----------------------------LOBOTOMIAAAAAAAAAAA----------------------------------------------")
+    endPoll = false
     playerlist.forEach(player=>{
         player.button=false
     })
+    res.send("ok")
 })
 app.get("/generateQRCode",async (req,res)=>{
     const url = "http://83.27.64.116:3000"
@@ -72,10 +111,17 @@ app.get("/adminMenu",(req,res)=>{
 })
 app.post("/takeLife",(req,res)=>{
     playerlist[req.body.id].lives-=1
+    res.send("ok")
 })
 app.post("/kickPlayer",(req,res)=>{
     playerlist = playerlist.filter((player)=> player.id!=req.body.id)
-    console.log(playerlist)
+    res.send("ok")
+})
+app.get("/playerStat",(req,res)=>{
+    const stat = req.query.stat
+    const id = req.query.id
+    if (stat=="lives") res.send(playerlist[id].lives)
+    if (stat=="button") res.send(playerlist[id].button)
 })
 app.get("/setSound",(req,res)=>{
     res.send("")
